8e3e0c5ee511914cf656cc31007d6e2d3bb76a91
Rachael Yoder <wayoder4@gmail.com>
2019-09-25 12:39:15 -0400

    added basic file structure

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// document.body.onload = setBoard();
+
+// function setBoard() {
+//     var container = document.createElement("div");
+//     var row = document.createElement("div");
+//     var col = document.createElement("div");
+
+//     container.setAttribute("class", "container");
+//     app.appendChild(container);
+// }
+
+var app = document.getElementById("app");
+var row = document.createElement("div");
+var col = document.createElement("div");
+
+app.setAttribute("class", "container");
+row.setAttribute("class", "row");
+col.setAttribute("class", "col-12");
