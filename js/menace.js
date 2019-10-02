20272e3f3e50c1b0a32080b342dabac0c4a186c6
Rachael Yoder <rachaelyoder@rachaels-mbp.lan>
2019-10-01 20:39:36 -0400

    working state gen

--- js/menace.js
+++ js/menace.js
@@ -1,50 +1,55 @@
 /* Global Variables */
-var states = { "[0,0,0]": [0, 0, 0] };
+var states = { "[0,0,0,0,0,0,0,0,0]": [0, 0, 0, 0, 0, 0, 0, 0, 0] };
 var keyStates = Object.keys(states);
 var overarchCount = 1, temp = 0;
 var savePos = 1;
+var count = 0;
 
-var possibleOut = 1, symCount = 1;
+var possibleOut = 1, symbol = 1;
+
+//Loop through all items in Object State
 for (var i = 0; i < possibleOut; i++) {
-    var count = 0;
     temp++;
-    //console.log({ keyStates });
-    //console.log(keyStates[i]);
     var arr = JSON.parse(keyStates[i]);
-    //console.log({ arr });
+
+    //Loop through array pulled from Object
     for (var j = 0; j < arr.length; j++) {
         var newArr = JSON.parse(JSON.stringify(arr));
-        //console.log({ "arr[j]": arr[j] });
-        if (arr[j] == 0) {
-            //console.log({ newArr, symCount });
-            newArr[j] = symCount;
-            //console.log({ newArr });
 
+        //If current array position is 0
+        if (arr[j] == 0) {
+            newArr[j] = symbol;
             var stateKey = "[" + newArr.join() + "]";
+
             if (stateKey == "[2,1,2]") {
-                console.log({ states, keyStates, stateKey, count, symCount, possibleOut, newArr, arr, i, j })
+                //console.log({ states, keyStates, stateKey, count, symbol, possibleOut, newArr, arr, i, j })
             }
+
+            //If current state is not in object, add it and increase count
             if (!(stateKey in states)) {
                 keyStates.push(stateKey);
                 states[stateKey] = newArr;
                 count++;
+                //console.log({ count });
             }
         }
     }
-    possibleOut += count;
-    //console.log({ possibleOut });
+    //Increase Outer loop exit condition
     if (i == (possibleOut - 1)) {
-        if (temp == overarchCount) {
-            savePos = possibleOut;
-
-            overarchCount = possibleOut - savePos;
-            temp = 0;
-            if (symCount == 1) {
-                symCount = 2;
-            } else {
-                symCount = 1;
-            }
+        possibleOut += count;
+        count = 0;
+        //console.log({ temp, overarchCount });
+        if (symbol == 1) {
+            symbol = 2;
+        } else {
+            symbol = 1;
         }
     }
+    //     if (temp == overarchCount) {
+    //         savePos = possibleOut;
+
+    //         overarchCount = possibleOut - savePos;
+    //         temp = 0;
+    // }
     //console.log({ states, keyStates });
 }
