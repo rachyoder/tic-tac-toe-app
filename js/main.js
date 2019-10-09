1af30f8615c371e378e4ebba496638899155e093
Rachael Yoder <wayoder4@gmail.com>
2019-10-09 10:10:31 -0400

    random bead selector functioning

--- js/main.js
+++ js/main.js
@@ -1,7 +1,47 @@
 var app = document.getElementById("app");
 var turn = 1;
 var checkGrid = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+let winCondit = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
+function turnCheck(e) {
+    //check for wins first - then run turn
+    var result = winCheck(checkGrid);
+    if (result == 1) {
+        console.log("X is the winner!");
+    } else if (result == 2) {
+        console.log("O is the winner!");
+    } else {
+        if (checkGrid[e.target.id] == 0) {
+            if (turn % 2 == 0) { //Turn for o
+                checkGrid[e.target.id] = 2;
+                this.innerHTML = "O";
+                turnCounter.innerHTML = "Current Turn: X";
+            } else { //Turn for x
+                checkGrid[e.target.id] = 1;
+                this.innerHTML = "X";
+                turnCounter.innerHTML = "Current Turn: O";
+            }
+            turn++;
+            var gameEnd = winCheck(checkGrid);
+            if (gameEnd == 1) {
+                turnCounter.innerHTML = "X is the Winner!";
+            } else if (gameEnd == 2) {
+                turnCounter.innerHTML = "O is the Winner!";
+            } else if (turn == 10) {
+                turnCounter.innerHTML = "Cat's Game! It's a tie!"
+            }
+        }
+    }
+}
 
 /* Board Setup */
 function createBoard(grid) {
@@ -13,53 +53,24 @@ function createBoard(grid) {
 
     /* Set up Boxes */
     for (var i = 0; i < 9; i++) {
-        var col = document.createElement("div");
+        var boxGrid = document.createElement("div");
         var letter = document.createElement("h2");
         letter.setAttribute("class", "manjariFont")
         var classStr = "col-4 display-4 manjariFont px-3 py-3 text-center border border-dark grid"
-        col.setAttribute('style', "height: 100px; width: 100px;");
-        col.setAttribute("id", i);
+        boxGrid.setAttribute('style', "height: 100px; width: 100px;");
+        boxGrid.setAttribute("id", i);
         letter.setAttribute("id", i);
-        col.setAttribute("class", classStr);
+        boxGrid.setAttribute("class", classStr);
 
         /* Checking for clicks */
-        col.addEventListener("click", function (e) {
-            //check for wins first - then run turn
-            var result = winCheck(checkGrid);
-            if (result == 1) {
-                console.log("X is the winner!");
-            } else if (result == 2) {
-                console.log("O is the winner!");
-            } else {
-                if (checkGrid[e.target.id] == 0) {
-                    if (turn % 2 == 0) { //Turn for o
-                        checkGrid[e.target.id] = 2;
-                        this.innerHTML = "O";
-                        turnCounter.innerHTML = "Current Turn: X";
-                    } else { //Turn for x
-                        checkGrid[e.target.id] = 1;
-                        this.innerHTML = "X";
-                        turnCounter.innerHTML = "Current Turn: O";
-                    }
-                    turn++;
-                    var gameEnd = winCheck(checkGrid);
-                    if (gameEnd == 1) {
-                        turnCounter.innerHTML = "X is the Winner!";
-                    } else if (gameEnd == 2) {
-                        turnCounter.innerHTML = "O is the Winner!";
-                    } else if (turn == 10) {
-                        turnCounter.innerHTML = "Cat's Game! It's a tie!"
-                    }
-                }
-            }
-        })
-        col.appendChild(letter);
-        row.appendChild(col);
+        boxGrid.addEventListener("click", turnCheck);
+        boxGrid.appendChild(letter);
+        row.appendChild(boxGrid);
     }
     boardContainer.appendChild(row);
 }
-
 /* Check Win Conditions */
+// Clean me!
 function winCheck(arr) {
     if (checkValues(arr[0], arr[1], arr[2]) == 1 || checkValues(arr[0], arr[1], arr[2]) == 2) {
         return checkValues(arr[0], arr[1], arr[2]);
@@ -77,7 +88,7 @@ function winCheck(arr) {
         return checkValues(arr[0], arr[4], arr[8]);
     } else if (checkValues(arr[2], arr[4], arr[6]) == 1 || checkValues(arr[2], arr[4], arr[6]) == 2) {
         return checkValues(arr[2], arr[4], arr[6]);
-    } 
+    }
 }
 
 function checkValues(a, b, c) {
@@ -97,9 +108,9 @@ function checkValues(a, b, c) {
 }
 /* Reset Game Button */
 function gameReset() {
-        checkGrid = [0,0,0,0,0,0,0,0,0];
-        turn = 1;
-        pageLayout();
+    checkGrid = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    turn = 1;
+    pageLayout();
 
 }
 
@@ -116,23 +127,43 @@ function pageLayout() {
             title.setAttribute("class", "display-4 mx-auto mb-6 markerFont");
             title.textContent = "Tic Tac Toe";
             div.appendChild(title);
+
         } else if (i == 1) { //Board Layout
             createBoard(div);
+
         } else if (i == 2) { //Turn Counter Setup
             var turnCounter = document.createElement("p");
             turnCounter.setAttribute("class", "mx-auto manjariFont");
             turnCounter.setAttribute("id", "turnCounter");
             turnCounter.textContent = "X Starts the Game...";
             div.appendChild(turnCounter);
-        } else if (i == 3) { //Reset Button
+
+        } else if (i == 3) { //Reset Button and Menace Mode
             var resetBtn = document.createElement("button");
             resetBtn.setAttribute("class", "btn btn-dark mx-auto");
             resetBtn.setAttribute("id", "resetBtn");
             resetBtn.innerHTML = "RESET GAME";
+            
+            var menaceToggleDiv = document.createElement("div");
+            var menaceToggleInput = document.createElement("input");
+            var menaceToggleLabel = document.createElement("label");
+            menaceToggleDiv.setAttribute("class", "custom-control custom-switch mx-auto");
+
+            menaceToggleInput.setAttribute("class", "custom-control-input ml-3");
+            menaceToggleInput.setAttribute("type", "checkbox");
+            menaceToggleInput.setAttribute("id", "toggleMenace");
+            menaceToggleInput.addEventListener("change", menaceMode);
+
+            menaceToggleLabel.setAttribute("class", "custom-control-label manjariFont");
+            menaceToggleLabel.setAttribute("for", "toggleMenace");
+            menaceToggleLabel.innerHTML = "Menace Mode";
+
+            menaceToggleDiv.appendChild(menaceToggleInput);
+            menaceToggleDiv.appendChild(menaceToggleLabel);
+            div.appendChild(menaceToggleDiv);
             resetBtn.addEventListener("click", gameReset);
             div.appendChild(resetBtn);
         }
         app.appendChild(div);
     }
-    //var grid = document.getElementsByClassName("grid");
 }
--- js/menace.js
+++ js/menace.js
@@ -1,51 +1,107 @@
-/* Global Variables */
-var states = { "[0,0,0,0,0,0,0,0,0]": [0, 0, 0, 0, 0, 0, 0, 0, 0] };
-var keyStates = Object.keys(states);
+/* Global variables */
+let STATES = { "[0,0,0,0,0,0,0,0,0]": { "new_arr": [0, 0, 0, 0, 0, 0, 0, 0, 0], "beads": [4, 4, 4, 4, 4, 4, 4, 4, 4], "BEAD_COUNT": 4 } };
+let KEY_STATES = Object.keys(STATES);
 
-var possibleOut = 1, symbol = 1, count = 0;
-var beadCount = 4, xTurn = 1;
+let POSSIBLE_OUT = 1, SYMBOL = 1, COUNT = 0;
+let BEAD_COUNT = 4, X_TURN = 1;
 
-var rotate = [2,5,8,1,4,7,0,3,6]; // 90 degree
+let ROTATE = [2, 5, 8, 1, 4, 7, 0, 3, 6]; // 90 degree
+
+
+/* ROTATION OF STATE */
 
 function rotation(state) {
-    for (var k = 0; k < rotate.length; k++) {
-        console.log (rotate[k]);
+    for (let l = 0; l < 3; l++) {
+        let rotate_state = [];
+        for (let m = 0; m < ROTATE.length; m++) {
+            rotate_state[m] = state[ROTATE[m]];
+        }
+        let rotate_state_key = `[${rotate_state}]`;
+        if (rotate_state_key in STATES) {
+            return false;
+        }
+    }
+    return true;
+}
+
+/* CHECK FOR WIN */
+
+function winChecker(state) {
+    if (winCheck(state) == 1 || winCheck(state) == 2) {
+        return false;
+    } else {
+        return true;
     }
 }
 
 /* STATE GENERATION */
 
 //Loop through all items in Object State
-for (var i = 0; i < possibleOut; i++) {
-    var arr = JSON.parse(keyStates[i]);
+for (let i = 0; i < POSSIBLE_OUT; i++) {
+    let arr = JSON.parse(KEY_STATES[i]);
 
     //Loop through array pulled from Object
-    for (var j = 0; j < arr.length; j++) {
-        var newArr = JSON.parse(JSON.stringify(arr));
-
+    for (let j = 0; j < arr.length; j++) {
+        let new_arr = JSON.parse(JSON.stringify(arr));
         //If current array position is 0
         if (arr[j] == 0) {
-            newArr[j] = symbol;
-            console.log({newArr});
-            var stateKey = "[" + newArr.join() + "]";
+            new_arr[j] = SYMBOL;
+            let bead_arr = JSON.parse(JSON.stringify(new_arr));
+            for (let k = 0; k < bead_arr.length; k++) {
+                if (bead_arr[k] == 0) {
+                    bead_arr[k] = BEAD_COUNT;
+                } else if (bead_arr[k] == 1) {
+                    bead_arr[k] = "X";
+                } else if (bead_arr[k] == 2) {
+                    bead_arr[k] = "O";
+                }
+            }
+            let state_key = "[" + new_arr.join() + "]";
 
             //If current state is not in object, add it and increase count
-            if (!(stateKey in states)) {
-                keyStates.push(stateKey);
-                states[stateKey] = newArr;
-                count++;
+            if (winChecker(new_arr)) {
+                if (!(state_key in STATES) && rotation(new_arr)) {
+                    KEY_STATES.push(state_key);
+                    STATES[state_key] = { new_arr, "beads": bead_arr, "BEAD_COUNT": BEAD_COUNT };
+                    COUNT++;
+                }
             }
         }
     }
+
     //Increase Outer loop exit condition
-    if (i == (possibleOut - 1)) {
-        possibleOut += count;
-        count = 0;
-        if (symbol == 1) {
-            symbol = 2; //O
+    if (i == (POSSIBLE_OUT - 1)) {
+        POSSIBLE_OUT += COUNT;
+        COUNT = 0;
+        if (SYMBOL == 1) {
+            SYMBOL = 2; //O
         } else {
-            symbol = 1;
-            xTurn += 2 //X
+            SYMBOL = 1;
+            X_TURN += 2; //X
+            BEAD_COUNT--;
         }
     }
+}
+
+/* RANDOM BEAD SELECTION */
+function randomBeadSelector(bead_state) {
+    let max = 0;
+    let total = 0
+    for (let x = 0; x < bead_state.length; x++) {
+        max += bead_state[x];
+    }
+    let bead_num = Math.floor((Math.random() * Math.floor(max)) + 1);
+    for (let y = 0; y < bead_state.length; y++) {
+        total += bead_state[y];
+        if (total >= bead_num) {
+            return y;
+        }
+    }
+}
+
+function menaceMode() {
+    let idx = randomBeadSelector(STATES["[0,0,0,0,0,0,0,0,0]"]["beads"]);
+    console.log({ idx });
+    document.getElementById(idx).click();
+
 }
